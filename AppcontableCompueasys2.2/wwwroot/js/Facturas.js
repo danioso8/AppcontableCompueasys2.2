6d0fa90e45e423447729b24d689e31f5137ee5e2
danioso8 <35614346+danioso8@users.noreply.github.com>
2023-02-20 23:38:28 -0500

    actuaclizacion 20/02 2

--- AppcontableCompueasys2.2/wwwroot/js/Facturas.js
+++ AppcontableCompueasys2.2/wwwroot/js/Facturas.js
@@ -8,27 +8,58 @@
 //          <td> ${_Producto.val()} </td>
 //          <td> ${_Producto.text()} </td>
 //          <td> ${_Producto.attr("Precio")} </td>
-                                                   
+
 //     </tr>
 //    `);
 //    let sumaT = parseInt(total) + parseInt(_Producto.attr("Precio"));
 //    $("#txtTotal").text(sumaT);
 //    con++;
-//}  
+//}
+
+
+
+
+
 
 //var con = 0;
-//function SeleccionarProducto() {
-//    let _Producto = $("#buscarProduc");
+//function SeleccionarProducto(p) {
+//    let _Producto = $(this).closest('.DaB').data(p);
 //    let total = $("#txtTotal").val() || 0;
 //    $("#tbProducto").append(`
 //      <tr>
 //          <td> ${_Producto.val()} </td>
 //          <td> ${_Producto.text()} </td>
 //          <td> ${_Producto.attr("Precio")} </td>
-                                                   
+
 //     </tr>
 //    `);
 //    let sumaT = parseInt(total) + parseInt(_Producto.attr("Precio"));
 //    $("#txtTotal").text(sumaT);
 //    con++;
-//}  
+//}
+
+
+
+$(document).redy(function () {
+    alert('hello')
+    consol.log('iniciando');
+    var con = 0;
+    $('#DaB').on('change', function () {
+
+        let _Producto = $(this).find("option:selected").text();
+        let total = $("#txtTotal").val() || 0;
+
+        $("#tbProducto").append(`
+          <tr>
+              <td>${_Producto.val()}</td>
+              <td>${_Producto.text()}</td>
+              <td>${_Producto.attr("Precio")}</td>
+
+         </tr>
+        `);
+        let sumaT = parseInt(total) + parseInt(_Producto.attr("Precio"));
+        $("#txtTotal").text(sumaT);
+        con++;
+    });
+    consol.log('terminando');
+    });
