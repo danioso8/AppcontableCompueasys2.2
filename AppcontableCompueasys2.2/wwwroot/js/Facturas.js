350bb938b3c1775d79d8319ae8386d724bea23c0
danioso8 <35614346+danioso8@users.noreply.github.com>
2023-02-24 04:00:23 -0500

    24/02 actualizacion facturas

--- AppcontableCompueasys2.2/wwwroot/js/Facturas.js
+++ AppcontableCompueasys2.2/wwwroot/js/Facturas.js
@@ -8,7 +8,34 @@
 
 $(document).ready(function () {
     
-    var con = 0;
+    var cont = 0;
+
+    $("#ProductoSeleccionado").on('change', function () {
+        var producto = $("#ProductoSeleccionado option:selected");
+        let precio = $(producto.attr("precio"));
+        let totalNeto = $("#txtTotal").val() || 0;
+        $("#txtSubTotal").val(producto.attr("precio"));
+        let cantidad = $("#Cantidad").val() || 2;
+        let total = precio * cantidad;
+        $("#tablaProducto").append(`
+                    <tr><td></td>
+                    <td>${producto.val()}</td>
+                    <td><input type="text" id="Cantidad" name="Cantidad" value="${producto.text()}"/></td>
+                    <td><input type="number" id="Cantidad" name="Cantidad" value="1" /></td>
+                    <td><input type="text" id="Cantidad" name="Cantidad" value="${producto.attr("precio")}"/></td>
+                    <td>${ parseFloat(producto.attr("precio")) }</td>
+                   
+                    </tr>
+                    `)
+        
+        let sumaTotal = parseInt(totalNeto) + parseInt(producto.attr("precio"));
+        $("#txtSubTotal").val(sumaTotal)
+        $("#txtTotal").val(sumaTotal)
+        cont++;
+    });
+   
+    
+
 
     fetch("/TipoDePagos/Get").then(res => {
 
@@ -59,7 +86,20 @@ $(document).ready(function () {
     //});
 
    
+  
+
+
+});
 
 
 
+
+
+var inputCantidad = $("#Cantidad");
+inputCantidad.keypress(function (event) {
+    if (event.keyCode == 13) {
+        let sumaTotal = parseInt(precio) * $("#Cantidad").val();
+        $("#txtSubTotal").val(sumaTotal)
+        $("#txtTotal").val(sumaTotal)
+    }
 });
