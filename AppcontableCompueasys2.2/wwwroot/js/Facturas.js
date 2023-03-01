538a3685a61396523356f3f92628bf324e38d30f
danioso8 <35614346+danioso8@users.noreply.github.com>
2023-03-01 02:17:21 -0500

    01/03 Ac 3

--- AppcontableCompueasys2.2/wwwroot/js/Facturas.js
+++ AppcontableCompueasys2.2/wwwroot/js/Facturas.js
@@ -4,7 +4,6 @@
 
 
 
-
 $(document).ready(function () {
 
  //fetch("/TipoDePagos/Get").then(res => {
@@ -41,42 +40,7 @@ $(document).ready(function () {
  //   });
 
 
-    $("#btnTerminarVentar").click(function () {
-        var Nfactura = $("#NumeroFactura").val();
-        var IdCliente = $("#txtDocumentoCliente").val();
-        var Total = $("#txtTotal").val();
-        var tipoPago = $("#cboTipoDocumentoVenta option:selected").val();
-        var Descuento = $("#Descuento").val();
-        var iva = $("#txtIGV").val();
-        var IdEmpresa = $("#IdEmpresa").val();
-        var IdDetalleC = $("#IdEmpresa").val();
-        var Observaciones = $("#IdEmpresa").val();
-        var Estado = $("#IdEmpresa").val();
-
-
-        $.ajax({
-            url: "/Facturas/CreateFactu",
-            type: "POST",
-            data: {
-                Nfactura: Nfactura,
-                IdCliente: IdCliente,
-                Total: Total,
-                tipoPago: tipoPago,
-                Descuento: Descuento,
-                iva: iva,
-                IdEmpresa: IdEmpresa,
-                IdDetalleC: IdDetalleC,
-                Observaciones: Observaciones,
-                Estado: Estado
-            },
-            success: function (result) {
-                // hacer algo con la respuesta del controlador
-            },
-            error: function (error) {
-                // manejar el error
-            }
-        });
-    });
+  
 
     var cont = 0;
 
---
+++ AppcontableCompueasys2.2/wwwroot/js/vistas/FacturaDatos.js
@@ -0,0 +1,47 @@
+﻿
+
+
+
+(document).ready(function () {
+   
+
+$("#btnTerminarVentar").click(function () {
+    var Nfactura = $("#NumeroFactura").val();
+    var IdCliente = $("#txtDocumentoCliente").val();
+    var Total = $("#txtTotal").val();
+    var tipoPago = $("#cboTipoDocumentoVenta option:selected").val();
+    var Descuento = $("#Descuento").val();
+    var iva = $("#txtIGV").val();
+    var IdEmpresa = $("#IdEmpresa").val();
+    var IdDetalleC = $("#IdEmpresa").val();
+    var Observaciones = $("#IdEmpresa").val();
+    var Estado = $("#IdEmpresa").val();
+
+
+    $.ajax({
+        url: "/Facturas/CreateFactu",
+        type: "POST",
+        data: {
+            Nfactura: Nfactura,
+            IdCliente: IdCliente,
+            Total: Total,
+            tipoPago: tipoPago,
+            Descuento: Descuento,
+            iva: iva,
+            IdEmpresa: IdEmpresa,
+            IdDetalleC: IdDetalleC,
+            Observaciones: Observaciones,
+            Estado: Estado
+        },
+        success: function (result) {
+            // hacer algo con la respuesta del controlador
+        },
+        error: function (error) {
+            // manejar el error
+        }
+    });
+});
+});
+
+
+
